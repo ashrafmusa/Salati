4dd456893f0cb18e74259723914f3584a139dd75
ashrafmusa <ashraf096849109@gmail.com>
2025-09-05 00:32:08 +0400

    Updated The vite.config.ts file to upload the favcon

--- vite.config.ts
+++ vite.config.ts
@@ -21,23 +21,23 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
             type: 'image/png',
             purpose: 'any',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
@@ -54,10 +54,10 @@ export default defineConfig({
         type: 'module',
       },
       includeAssets: [
-        'pwa-192x192.png',
-        'pwa-512x512.png',
+        'apple-touch-icon.png',
         'favicon.ico',
         'favicon-96x96.png',
+        'favicon.svg',
         'web-app-manifest-192x192.png',
         'web-app-manifest-512x512.png',
       ],
